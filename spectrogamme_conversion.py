33ef63c9aebfde0bd6ac9491d6a29ef247047fdd
Quentin <quentin.clodion@gmail.com>
2021-01-19 16:01:54 +0100

    Conversion to png

--- spectrogamme_conversion.py
+++ spectrogamme_conversion.py
@@ -3,17 +3,17 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
-#from wavio import readwav
 
 '''
 https://stackoverflow.com/questions/23377665/python-scipy-fft-wav-files
 https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
 '''
 
-metadata_inpath = os.path.join(os.getcwd(), 'dataset/rfcx-species-audio-detection/train_tp.csv')
-audio_inpath = os.path.join(os.getcwd(), 'dataset/rfcx-species-audio-detection/train')
+inpath = os.path.join(os.getcwd(), 'dataset', 'rfcx-species-audio-detection')
+metadata_inpath = os.path.join(inpath, 'train_tp.csv')
+audio_inpath = os.path.join(inpath, 'train')
 
-outpath = os.path.join(os.getcwd(), 'dataset/spectrogram-species-audio-detection')
+outpath = os.path.join(os.getcwd(), 'dataset', 'spectrogram-species-audio-detection')
 metadata_outpath = os.path.join(outpath, 'train_tp.csv')
 audio_outpath = os.path.join(outpath, 'train')
 
@@ -22,21 +22,23 @@ if not os.path.isdir(outpath):
 if not os.path.isdir(audio_outpath):
     os.mkdir(audio_outpath)
 
-with open(metadata_outpath, mode='w') as output_file:
+with open(metadata_outpath, mode='w', newline='') as output_csv_file:
     with open(metadata_inpath, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+                writer = csv.DictWriter(output_csv_file, row)
+                writer.writeheader()
                 line_count += 1
 
-            if line_count == 1:
-                data, sample = sf.read(os.path.join(audio_inpath, row["recording_id"] + ".flac"))
-                xx, frequency, bins, im = plt.specgram(data, Fs=sample)
-                plt.ylabel('Frequency [Hz]')
-                plt.xlabel('Time [sec]')
-                plt.show()
-
+            data, sample = sf.read(os.path.join(audio_inpath, row["recording_id"] + ".flac"))
+            start_audio = int(float(row["t_min"]) * 48000)
+            end_audio = int(float(row["t_max"]) * 48000)
+            xx, frequency, bins, im = plt.specgram([data[i] for i in range(start_audio, end_audio)], Fs=sample)
+            plt.axis('off')
+            plt.savefig(os.path.join(audio_outpath, row["recording_id"] + ".png"), bbox_inches='tight', pad_inches=0)
+            plt.close()
+            writer.writerow(row)
             line_count += 1
         print(f'Processed {line_count} lines.')
