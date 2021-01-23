96fc187e19b4543d1147073ddfb797893e15ef71
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-01-23 22:25:49 +0100

    train model + correctif

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import  tensorflow as tf
 from tensorflow.keras import  Model, Sequential
 from tensorflow.python.keras.layers import RandomFlip, RandomRotation, Dense, Flatten
 from tensorflow.keras.optimizers import SGD
-
+from dataset_param import  *
 
 def create_base_model(add_custom_layers_func) -> Model:
     m = Sequential()
@@ -12,7 +12,7 @@ def create_base_model(add_custom_layers_func) -> Model:
     add_custom_layers_func(m)
 
     m.add(Flatten())
-    m.add(Dense(252, activation=tf.keras.activations.softmax))
+    m.add(Dense(len(len_classes), activation=tf.keras.activations.softmax))
     m.compile(
         optimizer=SGD(),
         loss=tf.keras.losses.categorical_crossentropy,
@@ -24,7 +24,14 @@ def create_base_model(add_custom_layers_func) -> Model:
 def linearMod(Senq):
     pass
 
+def train_models(m:Model,x,y):
+    m.fit(
+        x,
+        y,
+        epochs=epch
+    )
 
 if __name__ == '__main__':
     m = create_base_model(linearMod)
     print(m)
+    #train_models(m,)
