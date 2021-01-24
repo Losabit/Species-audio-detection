8b6219386e82060801bce10fb65f174c53088495
Marvin <44065639+Mrenoir@users.noreply.github.com>
2021-01-24 13:06:25 +0100

    Little bug to fix  stay 1

--- linear.py
+++ linear.py
@@ -1,37 +1,40 @@
-import  tensorflow as tf
-from tensorflow.keras import  Model, Sequential
+import tensorflow as tf
+from tensorflow.keras import Model, Sequential
 from tensorflow.python.keras.layers import RandomFlip, RandomRotation, Dense, Flatten
 from tensorflow.keras.optimizers import SGD
-from dataset_param import  *
+from dataset_param import *
+import  numpy as np
+from utils import *
+
 
 def create_base_model(add_custom_layers_func) -> Model:
     m = Sequential()
-    m.add(RandomFlip("horizontal_and_vertical"))
-    m.add(RandomRotation(0.2))
-
     add_custom_layers_func(m)
 
     m.add(Flatten())
+    m.add(tf.keras.layers.Dense(10, tf.keras.activations.softmax))
     m.add(Dense(len(len_classes), activation=tf.keras.activations.softmax))
-    m.compile(
-        optimizer=SGD(),
-        loss=tf.keras.losses.categorical_crossentropy,
-        metrics=[tf.keras.metrics.categorical_accuracy]
-    )
+    m.compile(optimizer=tf.keras.optimizers.SGD(),
+                  loss=tf.keras.losses.categorical_crossentropy,
+                  metrics=tf.keras.metrics.categorical_accuracy)
+
 
     return m
+
 
 def linearMod(Senq):
     pass
 
-def train_models(m:Model,x,y):
-    m.fit(
-        x,
-        y,
-        epochs=epch
-    )
+
+def train_models(m, x, y):
+    m.fit(x, y, epochs=epch,batch_size=2)
+    m.summary()
+
 
 if __name__ == '__main__':
     m = create_base_model(linearMod)
+    (data, label) = load_data()
+    x , y  = build_x_y(data,label)
+    train_models(m,x,y)
+    print(x)
     print(m)
-    #train_models(m,)
