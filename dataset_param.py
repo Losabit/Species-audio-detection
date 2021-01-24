d1846225379c23878ff5a483060a87b2a5f9091f
PaulBenabou <paulbenabou@gmail.com>
2021-01-24 18:53:17 +0100

    various fix + image 100x100

--- dataset_param.py
+++ dataset_param.py
@@ -3,12 +3,13 @@ import os
 DATASET_DIRECTORY = os.path.join(os.getcwd(), 'dataset', 'spectrogram-species-audio-detection')
 DATASET_TRAIN_DIRECTORY = os.path.join(DATASET_DIRECTORY, 'train')
 DATASET_TRUE_TRAIN_CSV = os.path.join(DATASET_DIRECTORY, 'train_tp.csv')
-IMAGE_HEIGHT = 32
-IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 100
+IMAGE_WIDTH = 100
 len_classes = 24
 epch = 100
 KERNEL_REGULARIZERS = 0.0005
-lrTest =  0.01
+lrTest =  0.03
+batch_size = 1024
 momentumTest = 0.95
 ### PARAMS spectrogramm_conversion ###
 # Lié à IMAGE_WIDTH et IMAGE_HEIGHT
--- linear.py
+++ linear.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import Model, Sequential
-from tensorflow.python.keras.layers import RandomFlip, RandomRotation, Dense, Flatten
+from tensorflow.python.keras.layers import Flatten
 from tensorflow.keras.optimizers import SGD
 from dataset_param import *
 import numpy as np
@@ -16,7 +16,7 @@ def create_base_model(add_custom_layers_func) -> Model:
 
     m.compile(optimizer=tf.keras.optimizers.SGD(lr=lrTest),
               loss=tf.keras.losses.categorical_crossentropy,
-              metrics=tf.keras.metrics.categorical_accuracy)
+              metrics=["categorical_accuracy"])
 
     return m
 
@@ -24,9 +24,15 @@ def create_base_model(add_custom_layers_func) -> Model:
 def linear_mod(Seq):
     pass
 
+def add_mlp_layers(model):
+    model.add(tf.keras.layers.Flatten())
+    for _ in range(5):
+        model.add(tf.keras.layers.Dense(2048,activation=tf.keras.activations.linear))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Activation(activation=tf.keras.activations.tanh))
 
-def convNet(model):
-    model.add(tf.keras.layers.Reshape((32, 32, 4)))
+def add_convNet(model):
+    model.add(tf.keras.layers.Reshape((IMAGE_WIDTH, IMAGE_HEIGHT, 4)))
 
     model.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation=tf.keras.activations.tanh,
                                      kernel_regularizer=tf.keras.regularizers.l2(KERNEL_REGULARIZERS)))
@@ -47,12 +53,15 @@ def train_model(m: Model, x, y, x_val, y_val):
         y,
         validation_data=(x_val, y_val),
         epochs=epch,
-        batch_size=1024
+        batch_size=batch_size
     )
    return log
 
 
+
+
 if __name__ == '__main__':
+    print("loading data")
     (data, label) = load_data()
     train_data, val_data = split_array(data, 0.8)
     train_labels, val_labels = split_array(label, 0.8)
@@ -60,9 +69,8 @@ if __name__ == '__main__':
     val_data, val_labels = build_x_y(val_data, val_labels)
     print("data loaded")
 
-    model = create_base_model(convNet)
+    model = create_base_model(add_convNet)
     all_logs = [
         {"value": train_model(model, train_data, train_labels, val_data, val_labels), "title": "Conv mod"},
-
     ]
     plot_all_logs(all_logs)
