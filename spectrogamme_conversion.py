837a7243b642cf118feba44c6033eca78a3ca982
PaulBenabou <paulbenabou@gmail.com>
2021-01-27 20:09:56 +0100

    Added buggy data augmentation

--- spectrogamme_conversion.py
+++ spectrogamme_conversion.py
@@ -3,6 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 import soundfile as sf
 import math
+from audiomentations import Compose, AddGaussianNoise, AddGaussianSNR, FrequencyMask, PitchShift
 from PIL import Image
 from dataset_param import *
 from utils import count_csv_lines
@@ -11,6 +12,7 @@ initial_freq = 48000
 inpath = os.path.join(os.getcwd(), 'dataset', 'rfcx-species-audio-detection')
 metadata_inpath = os.path.join(inpath, 'train_tp.csv')
 audio_inpath = os.path.join(inpath, 'train')
+number_augmented_data_per_extract = 3
 
 
 def save_spectrogramm(data, sample, picture_path):
@@ -22,24 +24,40 @@ def save_spectrogramm(data, sample, picture_path):
     image.convert('RGB').resize((IMAGE_WIDTH, IMAGE_HEIGHT)).save(picture_path)
 
 
+def augment_data_and_save(data, sample, picture_path):
+    augment = Compose([
+        AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
+        AddGaussianSNR(),
+        PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
+        FrequencyMask()
+    ])
+
+    save_spectrogramm(data, sample, picture_path)
+
+    for i in range(number_augmented_data_per_extract):
+        augmented_samples = augment(samples=data, sample_rate=sample)
+        save_spectrogramm(augmented_samples, sample, picture_path.replace(".png", F"__{i}.png"))
+
+
 def process_and_save_spectrogramm(input_path, output_path, start_audio, end_audio):
     data, sample = sf.read(input_path)
     if DURATION_CUT != 0:
         nb_extraits = (end_audio - start_audio) / DURATION_CUT
         nb_extraits_int = int(math.floor(nb_extraits))
         for i in range(nb_extraits_int):
-            save_spectrogramm([data[j] for j in range(int((start_audio + i * DURATION_CUT) * initial_freq),
-                                                      int((start_audio + (i + 1) * DURATION_CUT) * initial_freq))],
-                              sample,  output_path + "_" + str(i) + ".png")
+            augment_data_and_save([data[j] for j in range(int((start_audio + i * DURATION_CUT) * initial_freq),
+                                                          int((start_audio + (i + 1) * DURATION_CUT) * initial_freq))],
+                                  sample, output_path + "_" + str(i) + ".png")
 
         if MINIMAL_DURATION < end_audio - (nb_extraits_int * DURATION_CUT) - start_audio:
-            save_spectrogramm([data[i] for i in range(int((start_audio + nb_extraits_int * DURATION_CUT) * initial_freq)
-                                                      , int(end_audio * initial_freq))],
-                              sample, output_path + "_" + str(nb_extraits_int) + ".png")
+            augment_data_and_save(
+                [data[i] for i in range(int((start_audio + nb_extraits_int * DURATION_CUT) * initial_freq)
+                                        , int(end_audio * initial_freq))],
+                sample, output_path + "_" + str(nb_extraits_int) + ".png")
 
     elif MINIMAL_DURATION < end_audio - start_audio:
-        save_spectrogramm([data[i] for i in range(int(start_audio * initial_freq), int(end_audio * initial_freq))],
-                          sample, output_path + "_0" + ".png")
+        augment_data_and_save([data[i] for i in range(int(start_audio * initial_freq), int(end_audio * initial_freq))],
+                              sample, output_path + "_0" + ".png")
 
 
 def create_spectro_dataset():
@@ -65,7 +83,7 @@ def create_spectro_dataset():
                     os.mkdir(class_directory)
 
             process_and_save_spectrogramm(os.path.join(audio_inpath, row["recording_id"] + ".flac"),
-                              os.path.join(class_directory, row["recording_id"] + "_" + str(line_count)),
-                              float(row["t_min"]), float(row["t_max"]))
+                                          os.path.join(class_directory, row["recording_id"] + "_" + str(line_count)),
+                                          float(row["t_min"]), float(row["t_max"]))
             line_count += 1
         print('100%')
